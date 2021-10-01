95088c7b51b7249c5cffcd8d8f36f3c87f44f89b
SHMF1385 <shmf.mousavi1385@gmail.com>
2021-10-01 16:30:15 +0330

    add Category and product models

--- digikala/kala/admin.py
+++ digikala/kala/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.Category)
+class CategotyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'in_stock']
+    list_filter = ['in_stock']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('name',)}
--- digikala/kala/models.py
+++ digikala/kala/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, db_index=True)
+    slug = models.SlugField(max_length=256, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=256)
+    price = models.DecimalField(max_digits=10, decimal_places=1)
+    in_stock = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+        # ordering = ("-created",)
+
+    def __str__(self):
+        return self.name
+
